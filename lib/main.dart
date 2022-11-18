ad8efa82b076ae8769ccbb98db7f5398d7419bb6
Abhinandkp <abhinandkp2001@gmail.com>
2022-11-18 16:25:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/presentation/main_page/widgets/screen_mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.black,
+          scaffoldBackgroundColor: backgroundColor,
+          textTheme: const TextTheme(
+            bodyText1: TextStyle(color: Colors.white),
+            bodyText2: TextStyle(color: Colors.white),
+          )),
+      home: ScreenMainPage(),
+    );
+  }
+}
---
+++ lib/presentation/main_page/widgets/screen_mainpage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/presentation/downloads/widgets/screen_downloads.dart';
+import 'package:netflix/presentation/fast_laughs/screen_fast_laughs.dart';
+import 'package:netflix/presentation/home/home_screen.dart';
+import 'package:netflix/presentation/main_page/widgets/bottom_nav.dart';
+import 'package:netflix/presentation/new_and_hot/screen_new_and_hot.dart';
+import 'package:netflix/presentation/search/screen_search.dart';
+
+class ScreenMainPage extends StatelessWidget {
+  ScreenMainPage({super.key});
+  final _pages = [
+    const ScreenHome(),
+    const ScreenNewAndHot(),
+    const ScreenFastLaughs(),
+    const ScreenSearch(),
+    const ScreenDownloads(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ValueListenableBuilder(
+          valueListenable: indexChangeNotifier,
+          builder: (context, int index, _) {
+            return _pages[index];
+          },
+        ),
+      ),
+      bottomNavigationBar: const BottomNavigationWidget(),
+    );
+  }
+}
